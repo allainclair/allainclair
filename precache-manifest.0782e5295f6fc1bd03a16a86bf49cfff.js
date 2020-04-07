7ed5d76640641430e65ccc320cb2af2fcc387960
allainclair <allainclair@gmail.com>
2020-04-07 03:01:47 -0300

    Updates

---
+++ precache-manifest.0782e5295f6fc1bd03a16a86bf49cfff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca6841c4dcf6fa99162ee018187d0e6",
+    "url": "/allainclair/index.html"
+  },
+  {
+    "revision": "597b85ddd33ffcc705bf",
+    "url": "/allainclair/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4315efac76c6039a2385",
+    "url": "/allainclair/static/js/2.239ac21d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/allainclair/static/js/2.239ac21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597b85ddd33ffcc705bf",
+    "url": "/allainclair/static/js/main.3d84a0b3.chunk.js"
+  },
+  {
+    "revision": "26dc7a42f5c12792cab0",
+    "url": "/allainclair/static/js/runtime-main.22a2f4a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/allainclair/static/media/logo.5d5d9eef.svg"
+  }
+]);
